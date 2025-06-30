49ca956d08d05e03754d76a952d85df3e52ebf6a
hassannwesh <hassannweshy12345@gmail.com>
2025-07-01 01:55:32 +0300

    add custom appbar and notesItem and searchIcon

---
+++ lib/widgets/custom_notes_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomNotesItem extends StatelessWidget {
+  const CustomNotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 18, bottom: 16, left: 16, ),
+      decoration: BoxDecoration(
+        color: Colors.orangeAccent,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text('Flutter Tips', style: TextStyle(color: Colors.black,fontSize: 24)),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: Text('Build your career with Hassan Nawishy  ', style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 18)),
+            ),
+            trailing: IconButton(onPressed: (){}, icon:const Icon(
+              Icons.delete,color: Colors.black,size: 30,
+            ),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text('May 20,2025', style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 18)),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custom_appBar.dart';
+import 'custom_notes_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(height: 50,),
+          CustomAppbar(),
+          CustomNotesItem(),
+        ],
+      ),
+    );
+  }
+}
