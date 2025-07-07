f61aa828ba248193325009332bd72190f3b7d96c
hassannwesh <hassannweshy12345@gmail.com>
2025-07-07 03:23:39 +0300

    registerAdapter

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -1,7 +1,8 @@
-
 import 'package:bloc/bloc.dart';
+import '../models/note_model.dart';
+part 'add_note_state.dart';
 
-class AddNotesState extends Cubit<AddNotesState>{
-  AddNotesState(super.initialState);
-
+class AddNoteCubit extends Cubit<AddNotesState> {
+  AddNoteCubit() : super(AddNoteInitial());
+  void addNote(NoteModel note) {}
 }
--- lib/cubits/add_note_state.dart
+++ lib/cubits/add_note_state.dart
@@ -1,11 +1,14 @@
 part of 'add_note_cubit.dart';
-abstract class AddNotesState{}
-class AddNoteInitial extends AddNotesState{}
-class AddNoteLoading extends AddNotesState{}
-class AddNoteSuccess extends AddNotesState{}
-class AddNoteFailure extends AddNotesState{
+
+abstract class AddNotesState {}
+
+class AddNoteInitial extends AddNotesState {}
+
+class AddNoteLoading extends AddNotesState {}
+
+class AddNoteSuccess extends AddNotesState {}
+
+class AddNoteFailure extends AddNotesState {
   final String errMassage;
   AddNoteFailure(this.errMassage);
-
-
 }
